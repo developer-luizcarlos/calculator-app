60fe4bb4d21476b5176f9c4a06d89c470c628ea3
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-11 10:48:50 -0300

    feat: create calculator header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,26 @@ import { ThemeContext } from "@/context/ThemeProvider";
 import { ThemeContextType } from "@/types/Theme";
 import { useContext } from "react";
 
+import Toggle from "@/components/Toggle/Toggle";
+
 const Home: React.FC = () => {
   const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
 
   const themeClassName = `theme-${theme}`;
 
-  return <div className={`calculator ${themeClassName}`}></div>;
+  return (
+    <div className={`calculator ${themeClassName}`}>
+      <div className="calculator__body">
+        <header className="calculator__header">
+          <h3 className="title--md">calc</h3>
+          <div className="toggle-theme-container">
+            <h4 className="title-sm">THEME</h4>
+            <Toggle />
+          </div>
+        </header>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
