9972ed614a56304751a592bc68a92e81b957a6c2
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-11 09:49:33 -0300

    feat: create calculator outermost component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { ThemeContext } from "@/context/ThemeProvider";
+import { ThemeContextType } from "@/types/Theme";
+import { useContext } from "react";
+
 const Home: React.FC = () => {
-  return <></>;
+  const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
+
+  const themeClassName = `theme-${theme}`;
+
+  return <div className={`calculator ${themeClassName}`}></div>;
 };
 
 export default Home;
