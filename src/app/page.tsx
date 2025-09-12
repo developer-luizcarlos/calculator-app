c6f3c266b809447c6ffbc49507f5f663a58df265
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-12 18:32:23 -0300

    feat: implement header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { ThemeContext } from "@/context/ThemeProvider";
 import { ThemeContextType } from "@/types/Theme";
 import { useContext } from "react";
 
+import Header from "@/components/Header/Header";
 import Screen from "@/components/Screen/Screen";
-import Toggle from "@/components/Toggle/Toggle";
 
 const Home: React.FC = () => {
   const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
@@ -15,13 +15,7 @@ const Home: React.FC = () => {
   return (
     <div className={`calculator ${themeClassName}`}>
       <div className="calculator__body">
-        <header className="calculator__header">
-          <h3 className="title--md">calc</h3>
-          <div className="toggle-theme-container">
-            <h4 className="title-sm">THEME</h4>
-            <Toggle />
-          </div>
-        </header>
+        <Header />
         <Screen value={0} />
       </div>
     </div>
