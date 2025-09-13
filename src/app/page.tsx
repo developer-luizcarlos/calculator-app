b780e8f6e40b138ed238f34e03884b23069915ec
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-13 13:25:58 -0300

    feat: implement calculator component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,7 @@
-"use client";
-
-import { ThemeContext } from "@/context/ThemeProvider";
-import { ThemeContextType } from "@/types/Theme";
-import { useContext } from "react";
-
-import Header from "@/components/Header/Header";
-import Screen from "@/components/Screen/Screen";
+import Calculator from "@/components/Calculator/Calculator";
 
 const Home: React.FC = () => {
-  const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
-
-  const themeClassName = `theme-${theme}`;
-
-  return (
-    <div className={`calculator ${themeClassName}`}>
-      <div className="calculator__body">
-        <Header />
-        <Screen value={0} />
-      </div>
-    </div>
-  );
+  return <Calculator />;
 };
 
 export default Home;
