3ab5887b3d4ce5007f0c7d2314aa024f29b3fb1e
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-13 14:21:23 -0300

    feat: implement button component

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -8,6 +8,7 @@ import { useContext } from "react";
 
 import Header from "@/components/Header/Header";
 import Screen from "@/components/Screen/Screen";
+import Button from "../Button/Button";
 
 const Calculator: React.FC = () => {
   const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
@@ -20,8 +21,28 @@ const Calculator: React.FC = () => {
         <Header />
         <Screen value={0} />
         <div className="calculator__keypad">
-          <div className="keypad__num-keys-container"></div>
-          <div className="keypad__calc-state-btns-container"></div>
+          <div className="keypad__num-keys-container">
+            <Button title="7" type="Numeric" onClick={() => ""} />
+            <Button title="8" type="Numeric" onClick={() => ""} />
+            <Button title="9" type="Numeric" onClick={() => ""} />
+            <Button title="del" type="Delete" onClick={() => ""} />
+            <Button title="4" type="Numeric" onClick={() => ""} />
+            <Button title="5" type="Numeric" onClick={() => ""} />
+            <Button title="6" type="Numeric" onClick={() => ""} />
+            <Button title="+" type="Operator" onClick={() => ""} />
+            <Button title="3" type="Numeric" onClick={() => ""} />
+            <Button title="2" type="Numeric" onClick={() => ""} />
+            <Button title="1" type="Numeric" onClick={() => ""} />
+            <Button title="-" type="Operator" onClick={() => ""} />
+            <Button title="." type="Numeric" onClick={() => ""} />
+            <Button title="0" type="Numeric" onClick={() => ""} />
+            <Button title="/" type="Numeric" onClick={() => ""} />
+            <Button title="x" type="Operator" onClick={() => ""} />
+          </div>
+          <div className="keypad__calc-state-btns-container">
+            <Button title="reset" type="Reset" onClick={() => ""} />
+            <Button title="=" type="Result" onClick={() => ""} />
+          </div>
         </div>
       </div>
     </div>
