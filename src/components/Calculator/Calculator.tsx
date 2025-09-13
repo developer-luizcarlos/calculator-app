5a7b4f2285ffd00d46b62e091ccfa78cdf552cce
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-13 13:25:23 -0300

    refact: turn calculator into a component

---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import "./styles.css";
+
+import { ThemeContext } from "@/context/ThemeProvider";
+import { ThemeContextType } from "@/types/Theme";
+import { useContext } from "react";
+
+import Header from "@/components/Header/Header";
+import Screen from "@/components/Screen/Screen";
+
+const Calculator: React.FC = () => {
+  const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
+
+  const themeClassName = `theme-${theme}`;
+
+  return (
+    <div className={`calculator ${themeClassName}`}>
+      <div className="calculator__body">
+        <Header />
+        <Screen value={0} />
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
