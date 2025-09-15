b5774bdb993bf6a9c7c46cd8b22efdacdb5b709a
Luiz Carlos Lima <developer.luizcarlos@outlook.com>
2025-09-15 12:51:57 -0300

    feat: create addValue function
    This function adds to the mathematical expression
    displayed on the calculator screen numeric or
    operator symbols, which is defined as a type given
    as a parameter, following rules to be included in
    the expression by its type value.

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -4,40 +4,118 @@ import "./styles.css";
 
 import { ThemeContext } from "@/context/ThemeProvider";
 import { ThemeContextType } from "@/types/Theme";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import Header from "@/components/Header/Header";
 import Screen from "@/components/Screen/Screen";
 import Button from "../Button/Button";
+
+import { isEmptyString, isLastCharNumber } from "@/utils/utils";
 
 const Calculator: React.FC = () => {
   const { theme } = useContext<ThemeContextType | null>(ThemeContext)!;
 
   const themeClassName = `theme-${theme}`;
 
+  const [expression, setExpression] = useState("0");
+
+  const addValue = (value: string, type: "Numeric" | "Operator") => {
+    switch (type) {
+      case "Operator":
+        if (isEmptyString(expression)) return;
+
+        if (isLastCharNumber(expression)) {
+          setExpression((exp) => exp + value);
+        }
+        break;
+      case "Numeric":
+        setExpression((exp) => (exp.trim() === "0" ? value : exp + value));
+    }
+  };
+
   return (
     <div className={`calculator ${themeClassName}`}>
       <div className="calculator__body">
         <Header />
-        <Screen value={0} />
+        <Screen value={expression} />
         <div className="calculator__keypad">
           <div className="keypad__num-keys-container">
-            <Button title="7" type="Numeric" onClick={() => ""} />
-            <Button title="8" type="Numeric" onClick={() => ""} />
-            <Button title="9" type="Numeric" onClick={() => ""} />
+            <Button
+              title="7"
+              type="Numeric"
+              onClick={() => addValue("7", "Numeric")}
+            />
+            <Button
+              title="8"
+              type="Numeric"
+              onClick={() => addValue("8", "Numeric")}
+            />
+            <Button
+              title="9"
+              type="Numeric"
+              onClick={() => addValue("9", "Numeric")}
+            />
             <Button title="del" type="Delete" onClick={() => ""} />
-            <Button title="4" type="Numeric" onClick={() => ""} />
-            <Button title="5" type="Numeric" onClick={() => ""} />
-            <Button title="6" type="Numeric" onClick={() => ""} />
-            <Button title="+" type="Operator" onClick={() => ""} />
-            <Button title="3" type="Numeric" onClick={() => ""} />
-            <Button title="2" type="Numeric" onClick={() => ""} />
-            <Button title="1" type="Numeric" onClick={() => ""} />
-            <Button title="-" type="Operator" onClick={() => ""} />
-            <Button title="." type="Numeric" onClick={() => ""} />
-            <Button title="0" type="Numeric" onClick={() => ""} />
-            <Button title="/" type="Numeric" onClick={() => ""} />
-            <Button title="x" type="Operator" onClick={() => ""} />
+            <Button
+              title="4"
+              type="Numeric"
+              onClick={() => addValue("4", "Numeric")}
+            />
+            <Button
+              title="5"
+              type="Numeric"
+              onClick={() => addValue("5", "Numeric")}
+            />
+            <Button
+              title="6"
+              type="Numeric"
+              onClick={() => addValue("6", "Numeric")}
+            />
+            <Button
+              title="+"
+              type="Operator"
+              onClick={() => addValue("+", "Operator")}
+            />
+            <Button
+              title="3"
+              type="Numeric"
+              onClick={() => addValue("3", "Numeric")}
+            />
+            <Button
+              title="2"
+              type="Numeric"
+              onClick={() => addValue("2", "Numeric")}
+            />
+            <Button
+              title="1"
+              type="Numeric"
+              onClick={() => addValue("1", "Numeric")}
+            />
+            <Button
+              title="-"
+              type="Operator"
+              onClick={() => addValue("-", "Operator")}
+            />
+            <Button
+              title="."
+              type="Numeric"
+              onClick={() => addValue(".", "Operator")}
+            />
+            <Button
+              title="0"
+              type="Numeric"
+              onClick={() => addValue("0", "Numeric")}
+            />
+            <Button
+              title="/"
+              type="Numeric"
+              onClick={() => addValue("/", "Operator")}
+            />
+            <Button
+              title="x"
+              type="Operator"
+              onClick={() => addValue("*", "Operator")}
+            />
           </div>
           <div className="keypad__calc-state-btns-container">
             <Button title="reset" type="Reset" onClick={() => ""} />
